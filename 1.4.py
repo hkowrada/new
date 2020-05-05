88bd84f1b467c870cc500a078359ec0bdd76e839
ashwi533 <ashwi533@gmail.com>
2020-05-05 16:40:01 +0530

    task2

---
+++ 1.4.py
@@ -0,0 +1,6 @@
+print('finding the volume of a sphere with diameter 12 cm.')
+print('Volume formula : V=4/3 * π * (r*r*r)')
+print('We already know that radius is half of diameter.Hence r=D/2')
+r = 12/2
+V = ((4/3)*(22/7)*(r*r*r))
+print('Volume is:', V)
